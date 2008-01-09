c890ea56d23d17d83954b112700144f10dd33875
SND\DotNetAddict_cp <SND\DotNetAddict_cp@0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5>
2008-01-09 19:40:42 +0000

    Initial first-cut of the Network Filtering Demo Solution
    git-svn-id: https://clinq.svn.codeplex.com/svn@1471 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

---
+++ NetworkFilteringDemo/NetworkLibrary/Implementations/Transaction.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace NetworkLibrary.Implementations
+{
+    public class Transaction : IEquatable<Transaction>, INotifyPropertyChanged
+    {
+        private string _employeeSource;
+        private int _quantity;
+        private double _amount;
+        private string _warehouseName;
+        private string _sku;
+
+        public string EmployeeSource
+        {
+            get
+            {
+                return _employeeSource;
+            }
+            set
+            {
+                _employeeSource = value;
+                NotifyChanged("EmployeeSource");
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return _quantity;
+            }
+            set
+            {
+                _quantity = value;
+                NotifyChanged("Quantity");
+            }
+        }
+
+        public double Amount
+        {
+            get
+            {
+                return _amount;
+            }
+            set
+            {
+                _amount = value;
+                NotifyChanged("Amount");
+            }
+        }
+
+        public string WarehouseName
+        {
+            get
+            {
+                return _warehouseName;
+            }
+            set
+            {
+                _warehouseName = value;
+                NotifyChanged("WarehouseName");
+            }
+        }
+
+        public string SKU
+        {
+            get
+            {
+                return _sku;
+            }
+            set
+            {
+                _sku = value;
+                NotifyChanged("SKU");
+            }
+        }
+
+        #region IEquatable<Transaction> Members
+
+        public bool Equals(Transaction other)
+        {
+            if ((other.WarehouseName == this.WarehouseName) &&
+                 (other.SKU == this.SKU) &&
+                 (other.Amount == this.Amount) &&
+                 (other.Quantity == this.Quantity) &&
+                 (other.EmployeeSource == this.EmployeeSource))
+                return true;
+            else
+                return false;
+        }
+
+        private void NotifyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
