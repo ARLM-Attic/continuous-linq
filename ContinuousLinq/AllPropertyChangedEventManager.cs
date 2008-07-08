a2a619f240b787f829064251136ef672c0374cfe
SND\DotNetAddict_cp <SND\DotNetAddict_cp@0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5>
2008-07-08 16:55:42 +0000

    Created property changed event manager for all property changes... first step in huge refactor.
    git-svn-id: https://clinq.svn.codeplex.com/svn@14267 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

---
+++ ContinuousLinq/AllPropertyChangedEventManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.ComponentModel;
+
+namespace ContinuousLinq
+{
+    public class AllPropertyChangedEventManager : WeakEventManager
+    {
+        protected override void StartListening(object source)
+        {
+            INotifyPropertyChanged pSource = (INotifyPropertyChanged)source;
+            pSource.PropertyChanged += HandlePropertyChanged;
+        }
+
+        protected override void StopListening(object source)
+        {
+            INotifyPropertyChanged pSource = (INotifyPropertyChanged)source;
+            pSource.PropertyChanged -= HandlePropertyChanged;
+        }
+
+        public void HandlePropertyChanged(object source, PropertyChangedEventArgs args)
+        {
+            DeliverEvent(source, args);
+        }
+
+        public static void AddListener(object source, IWeakEventListener listener)
+        {
+            AllPropertyChangedEventManager currentMgr =
+                AllPropertyChangedEventManager.CurrentManager;
+
+            currentMgr.ProtectedAddListener(source, listener);
+        }
+
+        public static void RemoveListener(object source, IWeakEventListener listener)
+        {
+            AllPropertyChangedEventManager currentMgr =
+                AllPropertyChangedEventManager.CurrentManager;
+
+            currentMgr.ProtectedRemoveListener(source, listener);
+        }
+
+        protected static AllPropertyChangedEventManager CurrentManager
+        {
+            get
+            {
+                return (AllPropertyChangedEventManager)WeakEventManager.GetCurrentManager(
+                    typeof(AllPropertyChangedEventManager));
+            }
+            set
+            {
+                WeakEventManager.SetCurrentManager(typeof(AllPropertyChangedEventManager),
+                    value);
+            }
+        }
+    }
+}
